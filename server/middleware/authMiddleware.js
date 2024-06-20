1e6bd29c3e8feef7af7a83218ab6819effad8533
kaora0 <Ka0rin@yandex.ru>
2024-06-20 20:44:51 +0300

    public

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = function (req, res, next) {
+    if (req.method === "OPTIONS") {
+        next()
+    }
+    try {
+        const token = req.headers.authorization.split(' ')[1] // типТокена токен14п4ц34
+        if (!token) {
+            return res.status(401).json({message: "Не авторизован"})
+        }
+        const decoded = jwt.verify(token, process.env.SECRET_KEY)
+        req.user = decoded
+        next()
+    } catch (e) {
+        res.status(401).json({message: "Ты не авторизован, чушка"})
+    }
+};
